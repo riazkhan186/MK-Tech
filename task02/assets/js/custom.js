5735d043e0a25505f322bbf7e991824d97ae9537
Riaz Uddin <riazbaiusc3s@gmail.com>
2022-06-06 18:16:20 +0600

    Latest File

--- task02/assets/js/custom.js
+++ task02/assets/js/custom.js
@@ -32,28 +32,4 @@
         nav: true,
         navText: ['<i class="fas fa-chevron-left"></i>', '<i class="fas fa-chevron-right"></i>'],
     });
-
-    // Go to Top
-    $(function () {
-        // Scroll Event
-        $(window).on("scroll", function () {
-            var scrolled = $(window).scrollTop();
-            if (scrolled > 600) $(".go-top").addClass("active");
-            if (scrolled < 600) $(".go-top").removeClass("active");
-        });
-        // Click Event
-        $(".go-top").on("click", function () {
-            $("html, body").animate({ scrollTop: "0" }, 500);
-        });
-    });
-
-
-    $(".odometer").appear(function (e) {
-        var odo = $(".odometer");
-        odo.each(function () {
-            var countNumber = $(this).attr("data-count");
-            $(this).html(countNumber);
-        });
-    });
-
 })(jQuery);
